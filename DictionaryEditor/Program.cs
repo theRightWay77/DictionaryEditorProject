2ef72eea81da543a99f782631eda66329b5c6f9a
theRightWay77 <Modnicazd77@gmail.com>
2024-07-02 17:48:29 +0300

    тестовый комит

--- DictionaryEditor/Program.cs
+++ DictionaryEditor/Program.cs
@@ -2,7 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+//Марат привет
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
